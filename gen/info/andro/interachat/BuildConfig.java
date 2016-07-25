675a7c2446cd41edbf8d28fd017b6e817e982b97
reevchris100 <reevchris100@gmail.com>
2016-07-25 23:21:15 +0530

    New changes

---
+++ gen/info/andro/interachat/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package info.andro.interachat;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/info/andro/interachat/other/WsConfig.java
@@ -0,0 +1,5 @@
+package info.andro.interachat.other;
+
+public class WsConfig {
+	public static final String URL_WEBSOCKET = "ws://192.168.43.142:8080/Interchat/chat?name=";
+}
